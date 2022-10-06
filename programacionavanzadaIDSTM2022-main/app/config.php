566bb03d7edc0afdbc514cfb83bfb1ac5de8154f
JonathanLeon2610 <111078357+JonathanLeon2610@users.noreply.github.com>
2022-10-06 11:03:02 -0500

    Add files via upload

---
+++ programacionavanzadaIDSTM2022-main/app/config.php
@@ -0,0 +1,12 @@
+<?php
+    session_start();
+
+if(!isset($_SESSION['global_token'])){
+    $_SESSION['global_token']=md5(uniqid(mt_rand(),true));
+
+}
+if(!defined('BASE_PATH')){
+    define('BASE_PATH','http://localhost/programacionavanzadaIDSTM2022-main/');
+}
+
+?>
