c511a0ea5e1836239852332c01268cdd3281b24b
JonathanLeon2610 <111078357+JonathanLeon2610@users.noreply.github.com>
2022-10-12 13:16:02 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -79,72 +79,86 @@
                                         <div class="container text-center">
                                             <div class="row">
                                                 <div class="col">
-                                                    <div class="card" style="width: 18rem;">
-                                                        <img src="..." class="card-img-top" alt="...">
+                                                    <div class="card border" style="width: 18rem;">
+                                                        <img src="../public/images/laptop.png" class="card-img-top" alt="...">
                                                         <div class="card-body">
                                                             <h5 class="card-title">Nombre del Producto</h5>
                                                             <p class="card-text">Descripcion del Producto</p>
-                                                            <a href="#" class="btn btn-primary">Detalles del Producto</a>
+                                                            <a href="#" class="btn btn-primary"><i class="bx bxs-comment-detail"></i> Detalles del Producto</a>
+                                                            <a href="" class="btn btn-secondary mt-2"><i class="mdi mdi-square-edit-outline"></i> Editar</a>
+                                                            <a href="" class="btn btn-danger mt-2"><i class="mdi mdi-delete"></i> Eliminar</a>
                                                         </div>
                                                     </div>
                                                 </div>
                                                 <div class="col">
-                                                    <div class="card" style="width: 18rem;">
-                                                        <img src="..." class="card-img-top" alt="...">
+                                                    <div class="card border" style="width: 18rem;">
+                                                        <img src="../public/images/laptop.png" class="card-img-top" alt="...">
                                                         <div class="card-body">
                                                             <h5 class="card-title">Nombre del Producto</h5>
                                                             <p class="card-text">Descripcion del Producto</p>
-                                                            <a href="#" class="btn btn-primary">Detalles del Producto</a>
+                                                            <a href="#" class="btn btn-primary"><i class="bx bxs-comment-detail"></i> Detalles del Producto</a>
+                                                            <a href="" class="btn btn-secondary mt-2"><i class="mdi mdi-square-edit-outline"></i> Editar</a>
+                                                            <a href="" class="btn btn-danger mt-2"><i class="mdi mdi-delete"></i> Eliminar</a>
                                                         </div>
                                                     </div>
                                                 </div>
                                                 <div class="col">
-                                                    <div class="card" style="width: 18rem;">
-                                                        <img src="..." class="card-img-top" alt="...">
+                                                    <div class="card border" style="width: 18rem;">
+                                                        <img src="../public/images/laptop.png" class="card-img-top" alt="...">
                                                         <div class="card-body">
                                                             <h5 class="card-title">Nombre del Producto</h5>
                                                             <p class="card-text">Descripcion del Producto</p>
-                                                            <a href="#" class="btn btn-primary">Detalles del Producto</a>
+                                                            <a href="#" class="btn btn-primary"><i class="bx bxs-comment-detail"></i> Detalles del Producto</a>
+                                                            <a href="" class="btn btn-secondary mt-2"><i class="mdi mdi-square-edit-outline"></i> Editar</a>
+                                                            <a href="" class="btn btn-danger mt-2"><i class="mdi mdi-delete"></i> Eliminar</a>
                                                         </div>
                                                     </div>
                                                 </div>
                                                 <div class="col">
-                                                    <div class="card" style="width: 18rem;">
-                                                        <img src="..." class="card-img-top" alt="...">
+                                                    <div class="card border" style="width: 18rem;">
+                                                        <img src="../public/images/laptop.png" class="card-img-top" alt="...">
                                                         <div class="card-body">
                                                             <h5 class="card-title">Nombre del Producto</h5>
                                                             <p class="card-text">Descripcion del Producto</p>
-                                                            <a href="#" class="btn btn-primary">Detalles del Producto</a>
+                                                            <a href="#" class="btn btn-primary"><i class="bx bxs-comment-detail"></i> Detalles del Producto</a>
+                                                            <a href="" class="btn btn-secondary mt-2"><i class="mdi mdi-square-edit-outline"></i> Editar</a>
+                                                            <a href="" class="btn btn-danger mt-2"><i class="mdi mdi-delete"></i> Eliminar</a>
                                                         </div>
                                                     </div>
                                                 </div>
                                                 <div class="col">
-                                                    <div class="card" style="width: 18rem;">
-                                                        <img src="..." class="card-img-top" alt="...">
+                                                    <div class="card border" style="width: 18rem;">
+                                                        <img src="../public/images/laptop.png" class="card-img-top" alt="...">
                                                         <div class="card-body">
                                                             <h5 class="card-title">Nombre del Producto</h5>
                                                             <p class="card-text">Descripcion del Producto</p>
-                                                            <a href="#" class="btn btn-primary">Detalles del Producto</a>
+                                                            <a href="#" class="btn btn-primary"><i class="bx bxs-comment-detail"></i> Detalles del Producto</a>
+                                                            <a href="" class="btn btn-secondary mt-2"><i class="mdi mdi-square-edit-outline"></i> Editar</a>
+                                                            <a href="" class="btn btn-danger mt-2"><i class="mdi mdi-delete"></i> Eliminar</a>
                                                         </div>
                                                     </div>
                                                 </div>
                                                 <div class="col">
-                                                    <div class="card" style="width: 18rem;">
-                                                        <img src="..." class="card-img-top" alt="...">
+                                                    <div class="card border" style="width: 18rem;">
+                                                        <img src="../public/images/laptop.png" class="card-img-top" alt="...">
                                                         <div class="card-body">
                                                             <h5 class="card-title">Nombre del Producto</h5>
                                                             <p class="card-text">Descripcion del Producto</p>
-                                                            <a href="#" class="btn btn-primary">Detalles del Producto</a>
+                                                            <a href="#" class="btn btn-primary"><i class="bx bxs-comment-detail"></i> Detalles del Producto</a>
+                                                            <a href="" class="btn btn-secondary mt-2"><i class="mdi mdi-square-edit-outline"></i> Editar</a>
+                                                            <a href="" class="btn btn-danger mt-2"><i class="mdi mdi-delete"></i> Eliminar</a>
                                                         </div>
                                                     </div>
                                                 </div>
                                                 <div class="col">
-                                                    <div class="card" style="width: 18rem;">
-                                                        <img src="..." class="card-img-top" alt="...">
+                                                    <div class="card border" style="width: 18rem;">
+                                                        <img src="../public/images/laptop.png" class="card-img-top" alt="...">
                                                         <div class="card-body">
                                                             <h5 class="card-title">Nombre del Producto</h5>
                                                             <p class="card-text">Descripcion del Producto</p>
-                                                            <a href="#" class="btn btn-primary">Detalles del Producto</a>
+                                                            <a href="#" class="btn btn-primary"><i class="bx bxs-comment-detail"></i> Detalles del Producto</a>
+                                                            <a href="" class="btn btn-secondary mt-2"><i class="mdi mdi-square-edit-outline"></i> Editar</a>
+                                                            <a href="" class="btn btn-danger mt-2"><i class="mdi mdi-delete"></i> Eliminar</a>
                                                         </div>
                                                     </div>
                                                 </div>
